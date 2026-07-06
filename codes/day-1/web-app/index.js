b7712008a13e36d8d37284981ed63bdfe4218308
joydip-git <joydip.mondal@gmail.com>
2026-07-06 11:26:35 +0530

    introductuon to JS: data types, operators, control flow statements

---
+++ codes/day-1/web-app/index.js
@@ -0,0 +1,57 @@
+var x = 10
+window.alert("value: " + x + ", data type: " + (typeof x))
+
+var y = 12.34
+window.alert("value: " + y + ", data type: " + (typeof y))
+
+var nameValue = "Joydip"
+window.alert("value: " + nameValue + ", data type: " + (typeof nameValue))
+
+var isManager = true
+window.alert("value: " + isManager + ", data type: " + (typeof isManager))
+
+isManager = "yes"
+window.alert("value: " + isManager + ", data type: " + (typeof isManager))
+
+var res1 = x + y
+window.alert(res1)
+
+res1 = x - y
+window.alert(res1)
+
+res1 = x * y
+window.alert(res1)
+
+window.alert(x++)
+
+x *= 5
+window.alert(x)
+
+window.alert(x > y ? 'greater' : 'lesser')
+
+var a = 0
+while (a < 5) {
+    window.alert(a)
+    a++
+}
+
+a = 0
+do {
+    window.alert(a);
+    a++
+} while (a < 5)
+
+a = 1
+switch (a) {
+    case 1:
+        window.alert(a)
+        break;
+
+    case 2:
+        window.alert(a)
+        break;
+
+    default:
+        window.alert('enter a proper choice')
+        break;
+}
