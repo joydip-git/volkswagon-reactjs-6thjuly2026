73f942f8876dc34d6129d6de7f6cb79d93e3a289
joydip-git <joydip.mondal@gmail.com>
2026-07-06 11:59:59 +0530

    calculator task

---
+++ codes/day-1/basics-js-app/index.js
@@ -0,0 +1,5 @@
+var strValue = window.prompt('enter value', 0) //'12'
+var first = Number(strValue) //'12' -> 12 or '12.34' -> 12.34
+
+//var x = parseInt(strValue) //'12' -> 12 or '12.34' -> 12
+//var x = parseFloat(strValue)//'12' -> 12.0 or '12.34' -> 12.34
