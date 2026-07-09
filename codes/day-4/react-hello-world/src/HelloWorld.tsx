21b02f110e44a5772d567f6279e0aa7abd226fb7
joydip-git <joydip.mondal@gmail.com>
2026-07-09 12:06:27 +0530

    React Component Details

--- codes/day-4/react-hello-world/src/HelloWorld.tsx
+++ codes/day-4/react-hello-world/src/HelloWorld.tsx
@@ -1,61 +1,32 @@
-import { createElement } from "react"
-
 export function HelloWorld() {
 
-    //code
-
-    //data
-    const message = 'Welcome to React JS'
-
-    //event handlers
+    const headerContent = 'Welcome to React JS'
+    const paraContent = 'this is how components are nested'
     const headerhoverhandler = function () {
         window.alert('hello...')
     }
+    const styles = {
+        backgroundColor: 'burlywood',
+        borderColor: 'burlywood',
+        borderRadius: '5px',
+        borderStyle: 'solid',
+        borderWidth: '1px',
+        color: 'black',
+        fontFamily: 'consolas'
+    }
 
-
-    // -------------------------
-    //template
-
-    //element
-    //const header = document.createElement('h2')
-
-    // binding data
-    // header.innerText = message
-
-    //event handler attachment
-    //header.addEventListener('mouseover', headerhoverhandler)
-    //header.onmouseover = headerhoverhandler
-
-    // header.id = 'mainHeader'
-
-    //style attributes
-    // header.style.backgroundColor = 'burlywood'
-    // header.style.fontFamily = 'Consolas'
-    // header.style.color = 'black'
-    // header.style.borderStyle = 'solid'
-    // header.style.borderRadius = '5px'
-    // header.style.borderWidth = '1px'
-    // header.style.borderColor = 'burlywood'
-    // header.style.textAlign = "center"
-
-    //using React API
-    const header = createElement(
-        'h2',
-        {
-            id: 'mainHeader',
-            onMouseOver: headerhoverhandler,
-            style: {
-                backgroundColor: 'burlywood',
-                borderColor: 'burlywood',
-                borderRadius: '5px',
-                borderStyle: 'solid',
-                borderWidth: '1px',
-                color: 'black',
-                fontFamily: 'Consolas',
-                textAlign: 'center'
-            }
-        },
-        message
+    return (
+        <div>
+            <h2 id="mainHeader"
+                style={styles}
+                onMouseOver={headerhoverhandler}
+            >
+                {headerContent}
+            </h2>
+            <br />
+            <span>
+                {paraContent}
+            </span>
+        </div>
     )
-    return header; //returning an React element
 }
